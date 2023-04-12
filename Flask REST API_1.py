6feb8c2404002db7f6f5baffee81d0bda5ddd8f3
SkywalkerGK <107118044+SkywalkerGK@users.noreply.github.com>
2023-04-12 22:53:43 +0700

    add-notFoundCity/city_id

--- Flask REST API_1.py
+++ Flask REST API_1.py
@@ -1,25 +1,31 @@
 #ทำงานฝั่ง Server เวลาที่ Cilent ส่งคำขอมา จะส่งข้อมูลที่อยู่ใน app.py ตอบกลับไปในรูปแบบ JSON
 from flask import Flask #1.สร้าง flask
-from flask_restful import Api,Resource #2.สร้าง api 3.สร้าง resource
+from flask_restful import Api,Resource,abort #2.สร้าง api 3.สร้าง resource
 
 app=Flask(__name__)
 api=Api(app)
 
 mycity={
-    "ชลบุรี":{"weather":"อากาศร้อนอบอ้าว","people":1500},
-    "ระยอง":{"weather":"ฝนตก","people":2000},
-    "กรุงเทพ":{"weather":"หนาว","people":5000},
+    1:{"name":"ชลบุรี","weather":"อากาศร้อนอบอ้าว","people":1500},
+    2:{"name":"ระยอง","weather":"ฝนตก","people":2000},
+    3:{"name":"กรุงเทพ","weather":"หนาว","people":5000},
 }
+
+#validate request
+def notFoundCity(city_id):
+    if city_id not in mycity:
+        abort(404,message = "ไม่พบข้อมูลที่คุณร้องขอ")
 
 #ออกแบบ Resorce
 class WeatherCity(Resource):
-    def get(self,name): #HTTP Method
-        return mycity[name]  #{"key":"value"}
+    def get(self,city_id): #HTTP Method
+        notFoundCity(city_id)
+        return mycity[city_id]  #{"key":"value"}
     def post(self,name):
         return{"data":"Creat Resource"+name}    
 
 #call เรียก Resorce ผ่าน api ทำให้ api มีก้อนข้อมูล
-api.add_resource(WeatherCity,"/weather/<string:name>") #เส้นทางในการเข้าถึง Resourse
+api.add_resource(WeatherCity,"/weather/<int:city_id>") #เส้นทางในการเข้าถึง Resourse
 
 if __name__ == "__main__":
     app.run(debug=True)
